6fe535603fdf83ec8fc7d83c08699f849a50cb7b
haloandrei <haloandrei@yahoo.com>
2021-03-28 19:37:17 +0300

    feat: test case add student valid

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -6,8 +6,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import ssvv.example.domain.Student;
+import ssvv.example.repository.NotaXMLRepository;
 import ssvv.example.repository.StudentFileRepository;
+import ssvv.example.repository.StudentXMLRepository;
+import ssvv.example.repository.TemaXMLRepository;
+import ssvv.example.service.Service;
+import ssvv.example.validation.NotaValidator;
 import ssvv.example.validation.StudentValidator;
+import ssvv.example.validation.TemaValidator;
 import ssvv.example.validation.Validator;
 
 import java.util.Iterator;
@@ -29,6 +35,24 @@ public class AppTest
         StudentFileRepository rep = new StudentFileRepository(validator,"studenti.txt");
         rep.findAll().forEach(student -> { studentCounter.getAndSet(studentCounter.get() + 1);});
         assertTrue( studentCounter.get() == 2 );
+    }
+    @Test
+    public void AddStudentSuccessfully()
+    {
+        AtomicReference<Integer> studentCounter = new AtomicReference<>(0);
+        Validator studentValidator = new StudentValidator();
+        Validator temaValidator = new TemaValidator();
+        Validator notaValidator = new NotaValidator();
+
+        StudentXMLRepository studRep = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository temaRep = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository notaRep = new NotaXMLRepository(notaValidator, "note.xml");
+        Service service = new Service(studRep,temaRep,notaRep);
+
+        String id = "alksdjnfTest12";
+        service.deleteStudent(id);
+        assertTrue( service.saveStudent(id,"TestGood",231) == 1 );
+
     }
     @Test
     public void FirstStudentIsConsistent()
