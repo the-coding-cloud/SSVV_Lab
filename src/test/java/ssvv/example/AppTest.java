fca420f899cae28ebac937272aecdc2772021427
the-coding-cloud <oana.nourescu@gmail.com>
2021-03-29 21:34:45 +0300

    BVA group fixed

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -101,25 +101,16 @@ public class AppTest
     }
 
     @Test
-    public void AddStudent_GroupLessThan110()
+    public void AddStudent_GroupLessThan111()
     {
         String id = "12";
         String name = "Test";
         service.deleteStudent(id);
-        assertEquals(0, service.saveStudent(id, name, 99));
+        assertEquals(0, service.saveStudent(id, name, 110));
     }
 
     @Test
-    public void AddStudent_GroupEqualTo110()
-    {
-        String id = "12";
-        String name = "Test";
-        service.deleteStudent(id);
-        assertEquals(1, service.saveStudent(id, name, 110));
-    }
-
-    @Test
-    public void AddStudent_GroupMoreThan110()
+    public void AddStudent_GroupEqualTo111()
     {
         String id = "12";
         String name = "Test";
@@ -128,7 +119,25 @@ public class AppTest
     }
 
     @Test
-    public void AddStudent_GroupLessThan938()
+    public void AddStudent_GroupMoreThan111()
+    {
+        String id = "12";
+        String name = "Test";
+        service.deleteStudent(id);
+        assertEquals(1, service.saveStudent(id, name, 112));
+    }
+
+    @Test
+    public void AddStudent_GroupLessThan937()
+    {
+        String id = "12";
+        String name = "Test";
+        service.deleteStudent(id);
+        assertEquals(1, service.saveStudent(id, name, 936));
+    }
+
+    @Test
+    public void AddStudent_GroupEqualTo937()
     {
         String id = "12";
         String name = "Test";
@@ -137,21 +146,12 @@ public class AppTest
     }
 
     @Test
-    public void AddStudent_GroupEqualTo938()
+    public void AddStudent_GroupMoreThan937()
     {
         String id = "12";
         String name = "Test";
         service.deleteStudent(id);
-        assertEquals(1, service.saveStudent(id, name, 938));
-    }
-
-    @Test
-    public void AddStudent_GroupMoreThan938()
-    {
-        String id = "12";
-        String name = "Test";
-        service.deleteStudent(id);
-        assertEquals(0, service.saveStudent(id, name, 939));
+        assertEquals(0, service.saveStudent(id, name, 938));
     }
 
 }
